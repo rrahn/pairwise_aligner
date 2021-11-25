bfebcb8e3c9d054b658d8e298231b992775cafa0
rrahn <rene.rahn@fu-berlin.de>
2021-11-25 10:18:22 +0100

    [TEST] Add test for simd global affine alignment.

---
+++ test/unit/affine/alignment_global_affine_simd_test.cpp
@@ -0,0 +1,48 @@
+// -----------------------------------------------------------------------------------------------------
+// Copyright (c) 2006-2021, Knut Reinert & Freie Universität Berlin
+// Copyright (c) 2016-2021, Knut Reinert & MPI für molekulare Genetik
+// This file may be used, modified and/or redistributed under the terms of the 3-clause BSD-License
+// shipped with this file and also available at: https://github.com/rrahn/pairwise_aligner/blob/master/LICENSE.md
+// -----------------------------------------------------------------------------------------------------
+
+#include <gtest/gtest.h>
+
+#include <string_view>
+
+#include <pairwise_aligner/affine/affine_dp_algorithm.hpp>
+
+TEST(affine_test, all_match)
+{
+    seqan::pairwise_aligner::pairwise_aligner_affine<> aligner{};
+
+    std::string_view seq1{"ACGTGACTGACACTACGACT"};
+    std::string_view seq2{"ACGTGACTGACACTACGACT"};
+
+    std::vector<std::string_view> seq1_collection{};
+    seq1_collection.resize(8, seq1);
+
+    std::vector<std::string_view> seq2_collection{};
+    seq2_collection.resize(8, seq2);
+
+    auto res = aligner.compute_simd(seq1_collection, seq2_collection);
+    for (auto i = 0; i < 8; ++i)
+        EXPECT_EQ(res[i], 80);
+}
+
+TEST(affine_test, all_mismatch)
+{
+    seqan::pairwise_aligner::pairwise_aligner_affine<> aligner{};
+
+    std::string_view seq1{"AAAAAAAAAA"};
+    std::string_view seq2{"TTTTTTTTTT"};
+
+    std::vector<std::string_view> seq1_collection{};
+    seq1_collection.resize(8, seq1);
+
+    std::vector<std::string_view> seq2_collection{};
+    seq2_collection.resize(8, seq2);
+
+    auto res = aligner.compute_simd(seq1_collection, seq2_collection);
+    for (auto i = 0; i < 8; ++i)
+        EXPECT_EQ(res[i], -40);
+}
