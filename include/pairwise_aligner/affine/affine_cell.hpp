0ce108a8f7ddf4b986c996c1511f4e786330be4f
rrahn <rene.rahn@fu-berlin.de>
2021-11-25 16:00:31 +0100

    [FEATURE] Adds special cell type for alignment with affine gaps.

---
+++ include/pairwise_aligner/affine/affine_cell.hpp
@@ -0,0 +1,43 @@
+// -----------------------------------------------------------------------------------------------------
+// Copyright (c) 2006-2021, Knut Reinert & Freie Universität Berlin
+// Copyright (c) 2016-2021, Knut Reinert & MPI für molekulare Genetik
+// This file may be used, modified and/or redistributed under the terms of the 3-clause BSD-License
+// shipped with this file and also available at: https://github.com/rrahn/pairwise_aligner/blob/master/LICENSE.md
+// -----------------------------------------------------------------------------------------------------
+
+/*!\file
+ * \brief Provides seqan::pairwise_aligner::affine_cell_row and seqan::pairwise_aligned::affine_cell_column.
+ * \author Rene Rahn <rahn AT molgen.mpg.de>
+ */
+
+#pragma once
+
+#include <utility>
+
+#include <pairwise_aligner/type_traits.hpp>
+
+namespace seqan::pairwise_aligner
+{
+inline namespace v1
+{
+
+template <typename score_t, dp_vector_order order>
+struct affine_cell : public dp_cell_base<order>,
+                     public std::pair<score_t, score_t>
+{
+    using base_t = std::pair<score_t, score_t>;
+    using score_type = score_t;
+
+    using base_t::base_t;
+};
+
+// template <typename score_t = int32_t>
+// struct affine_cell_row : public affine_cell_base<score_t, dp_vector_order::row>
+// {};
+
+// template <typename score_t = int32_t>
+// struct affine_cell_column : public affine_cell_base<score_t, dp_vector_order::column>
+// {};
+
+} // inline namespace v1
+}  // namespace seqan::pairwise_aligner
