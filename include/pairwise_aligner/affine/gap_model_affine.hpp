40e3e49ba6a586d7029049c065a1f6a063526183
rrahn <rene.rahn@fu-berlin.de>
2021-11-25 15:59:22 +0100

    [FEATURE] Adds affine gap model.

---
+++ include/pairwise_aligner/affine/gap_model_affine.hpp
@@ -0,0 +1,28 @@
+// -----------------------------------------------------------------------------------------------------
+// Copyright (c) 2006-2021, Knut Reinert & Freie Universität Berlin
+// Copyright (c) 2016-2021, Knut Reinert & MPI für molekulare Genetik
+// This file may be used, modified and/or redistributed under the terms of the 3-clause BSD-License
+// shipped with this file and also available at: https://github.com/rrahn/pairwise_aligner/blob/master/LICENSE.md
+// -----------------------------------------------------------------------------------------------------
+
+/*!\file
+ * \brief Provides seqan::pairwise_aligner::gap_model_affine.hpp.
+ * \author Rene Rahn <rahn AT molgen.mpg.de>
+ */
+
+#pragma once
+
+namespace seqan::pairwise_aligner
+{
+inline namespace v1
+{
+
+template <typename score_t>
+struct gap_model_affine
+{
+    score_t gap_open_score{};
+    score_t gap_extension_score{};
+};
+
+} // inline namespace v1
+}  // namespace seqan::pairwise_aligner
