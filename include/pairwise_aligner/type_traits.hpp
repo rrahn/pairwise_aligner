81feb89cad004de83265a38477d9dcb1b3e0a95b
rrahn <rene.rahn@fu-berlin.de>
2021-11-25 15:59:00 +0100

    [FEATURE] Adds basic type traits class to define some common types and type traits.

---
+++ include/pairwise_aligner/type_traits.hpp
@@ -0,0 +1,53 @@
+// -----------------------------------------------------------------------------------------------------
+// Copyright (c) 2006-2021, Knut Reinert & Freie Universität Berlin
+// Copyright (c) 2016-2021, Knut Reinert & MPI für molekulare Genetik
+// This file may be used, modified and/or redistributed under the terms of the 3-clause BSD-License
+// shipped with this file and also available at: https://github.com/rrahn/pairwise_aligner/blob/master/LICENSE.md
+// -----------------------------------------------------------------------------------------------------
+
+/*!\file
+ * \brief Provides various helper templates to access type properties.
+ * \author Rene Rahn <rahn AT molgen.mpg.de>
+ */
+
+#pragma once
+
+#include <cstdint>
+
+#include <seqan3/std/concepts>
+
+namespace seqan::pairwise_aligner
+{
+inline namespace v1
+{
+
+/*!\brief The order of the accessed vector.
+ *
+ * This enum is used to disambiguate cells from a column or a row vector of the underlying DP matrix.
+ */
+enum struct dp_vector_order
+{
+    row,
+    column
+};
+
+//!\brief To check if a type is a dp cell.
+template <dp_vector_order _order>
+struct dp_cell_base
+{};
+
+// CPO?
+template <typename dp_cell_t>
+inline constexpr bool is_row_cell_v = false;
+
+template <std::derived_from<dp_cell_base<dp_vector_order::row>> dp_cell_t>
+inline constexpr bool is_row_cell_v<dp_cell_t> = true;
+
+template <typename dp_cell_t>
+inline constexpr bool is_column_cell_v = false;
+
+template <std::derived_from<dp_cell_base<dp_vector_order::column>> dp_cell_t>
+inline constexpr bool is_column_cell_v<dp_cell_t> = true;
+
+} // inline namespace v1
+}  // namespace seqan::pairwise_aligner
