338f0ee2550f694f4b58c5db9e5d6eafb67147fe
rrahn <rene.rahn@fu-berlin.de>
2021-12-01 13:11:55 +0100

    [TEST] Add basic test for aligner configuration rules.

---
+++ test/unit/configuration/configuration_test.cpp
@@ -0,0 +1,32 @@
+// -----------------------------------------------------------------------------------------------------
+// Copyright (c) 2006-2021, Knut Reinert & Freie Universität Berlin
+// Copyright (c) 2016-2021, Knut Reinert & MPI für molekulare Genetik
+// This file may be used, modified and/or redistributed under the terms of the 3-clause BSD-License
+// shipped with this file and also available at: https://github.com/rrahn/pairwise_aligner/blob/master/LICENSE.md
+// -----------------------------------------------------------------------------------------------------
+
+#include <gtest/gtest.h>
+
+#include <string_view>
+
+#include <pairwise_aligner/score_model/score_model_unitary.hpp>
+#include <pairwise_aligner/configuration/configure_aligner.hpp>
+#include <pairwise_aligner/configuration/gap_model_affine.hpp>
+#include <pairwise_aligner/configuration/score_model_unitary.hpp>
+
+TEST(configuration_test, prototype)
+{
+    namespace pa = seqan::pairwise_aligner;
+
+    auto aligner = pa::cfg::configure_aligner(
+        pa::cfg::gap_model_affine(
+            pa::cfg::score_model_unitary(4, -5),
+            -10, -1
+        )
+    );
+
+    std::string_view seq1{"ACGTGACTGACACTACGACT"};
+    std::string_view seq2{"ACGTGACTGACACTACGACT"};
+
+    EXPECT_EQ((aligner.compute(seq1, seq2)), 80);
+}
