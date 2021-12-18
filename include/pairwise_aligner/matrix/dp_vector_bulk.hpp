f93d8603c018e14d2cb9d21a76ce286e0f945bb7
rrahn <rahn@molgen.mpg.de>
2021-12-18 12:41:57 +0100

    [FEATURE] Use seqan3 to_simd view for efficient transpose of sequence memory.

--- include/pairwise_aligner/matrix/dp_vector_bulk.hpp
+++ include/pairwise_aligner/matrix/dp_vector_bulk.hpp
@@ -14,6 +14,9 @@
 
 #include <seqan3/std/algorithm>
 #include <seqan3/std/ranges>
+#include <seqan3/utility/container/aligned_allocator.hpp>
+#include <seqan3/utility/simd/views/to_simd.hpp>
+#include <seqan3/alphabet/adaptation/char.hpp>
 
 namespace seqan::pairwise_aligner
 {
@@ -63,35 +66,37 @@ public:
         size_t const sequence_count = std::ranges::distance(sequence);
         size_t max_sequence_size = 0;
 
+        using optional_t = std::ranges::range_value_t<sequence_t>;
+        using seq_view_t = typename optional_t::value_type;
+
+        std::vector<seq_view_t, seqan3::aligned_allocator<seq_view_t, alignof(simd_t)>> sequences{};
+        sequences.reserve(sequence_count);
+
         std::ranges::for_each(sequence, [&] (auto && optional_sequence)
         {
-            if (optional_sequence.has_value())
+            if (optional_sequence.has_value()) {
                 max_sequence_size = std::max<size_t>(max_sequence_size, std::ranges::distance(*optional_sequence));
+                sequences.push_back(*optional_sequence);
+            }
         });
 
-        // we get the wrong result type here.
-        // so this must be a type set by configurator.
-        // using simd_t = typename value_type::score_type;
         using score_t = typename simd_t::value_type;
+        using simd_score_t = typename simd_t::simd_type;
 
         constexpr size_t bit_count = sizeof(score_t) * 8;
         constexpr score_t padding_mask = static_cast<score_t>(1 << (bit_count - 1));
 
-        std::vector<simd_t> simd_sequence{};
-        simd_sequence.resize(max_sequence_size, simd_t{padding_mask});
+        std::vector<simd_t, seqan3::aligned_allocator<simd_t, alignof(simd_t)>> simd_sequence{};
+        simd_sequence.reserve(max_sequence_size);
 
-        for (size_t i = 0; i < sequence_count; ++i)
-        {
-            if (!sequence[i].has_value())
-                continue;
+        auto simd_view = sequences | seqan3::views::to_simd<simd_score_t>(padding_mask);
 
-            size_t j = 0;
-            for (auto it = std::ranges::begin(*sequence[i]); it != std::ranges::end(*sequence[i]); ++it, ++j)
-                simd_sequence[j][i] = static_cast<score_t>(*it);
+        for (auto && simd_vector_chunk : simd_view) {
+            for (auto && simd_vector : simd_vector_chunk) {
+                simd_sequence.emplace_back(std::move(simd_vector));
+            }
         }
-
         return _dp_vector.initialise(std::move(simd_sequence), std::forward<initialisation_strategy_t>(init_strategy));
-        // return simd_sequence;
     }
 };
 
