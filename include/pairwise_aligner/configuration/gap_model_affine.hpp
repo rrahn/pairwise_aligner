38e562e50a5ae42e8f4e0ebd1ea53b0d92f78176
rrahn <rene.rahn@fu-berlin.de>
2022-01-12 13:38:25 +0100

    [REFACTOR] Make algorithm type independent of order of given policies.

--- include/pairwise_aligner/configuration/gap_model_affine.hpp
+++ include/pairwise_aligner/configuration/gap_model_affine.hpp
@@ -54,10 +54,9 @@ struct traits
     using dp_cell_row_type = affine_cell<score_t, dp_vector_order::row>;
 
     // Offer some overload for the column type.
-    template <template <typename ...> typename dp_template_t, typename dp_score_model_t, typename result_factory_t>
+    template <template <typename ...> typename dp_template_t, typename ...policies_t>
     using dp_kernel_type = affine_dp_algorithm<dp_template_t,
-                                               dp_score_model_t,
-                                               result_factory_t,
+                                               policies_t...,
                                                gap_model_type,
                                                initialisation_rule,
                                                trailing_gap_setting>;
