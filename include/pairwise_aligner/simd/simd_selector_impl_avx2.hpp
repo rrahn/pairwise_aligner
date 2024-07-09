0a7c3e0d327fd333853d048eda2e4f9644ae1d6d
rrahn <rene.rahn@fu-berlin.de>
2024-07-09 15:13:14 +0200

    Fix implementation of SIMD select for AVX2 using 8-bit operand types with in-lane shuffle instructions.

--- include/pairwise_aligner/simd/simd_selector_impl_avx2.hpp
+++ include/pairwise_aligner/simd/simd_selector_impl_avx2.hpp
@@ -62,117 +62,80 @@ struct simd_selector<simd_offset_t, selector_tag<operand_count, 8, 256>>
 
     using scalar_offset_t = typename simd_offset_t::value_type;
 
-    static constexpr simd_offset_t lane_selector{static_cast<scalar_offset_t>(0x80)};
-
-    __m256i first_mask;
-    __m256i second_mask;
+    __m256i keys_low;
+    __m256i keys_high;
 
     simd_selector() = delete;
+
+    /**
+     * @brief Construct a new simd selector object
+     *
+     * @param[in] offsets The offsets to select the values from the input array from.
+     *
+     * The select operation utilises the AVX2 shuffle instruction for 8-bit operand types to select the values from the
+     * input vector. Hence the selector can address at most 32 values in a single operation.
+     * Furthermore, the shuffle instruction only selects within a 128-bit lane requiring to split the selection
+     * keys into two parts for the two address spaces 0-15 and 16-31. These are represented by `keys_low` and
+     * `keys_high` respectively.
+     *
+     * To initialize these vectors the offsets are first truncated to the lower 5 bits (index range 0-31) and then
+     * the MSB is set to 1 for keys greater than 15. Using this mask the keys for the low and high address space are
+     * marked accordingly, i.e. in keys_low all entries with an index >= 16 have the MSB set to 1 and in keys_high
+     * all entries with an index < 16 have the MSB set to 1.
+     * The MSB is later used to set the corresponding entry to 0 (see documentation of `_mm256_shuffle_epi8` for
+     * details).
+     */
     simd_selector(simd_offset_t const & offsets) noexcept
     {
-        // Check that no value is greater than or equal to 128 (0x80).
-        // assert([&](){
-        //     for (size_t i = 0; i < simd_offset_t::size_v; ++i) {
-        //         if (offsets[i] >= 0x80)
-        //             return false;
-        //     }
-        //     return true;
-        // }());
-        // what happens:
-        // index:[0, 1, 2, 3 ... 15][16, 17, 18, ... 31] => one register
-        // address -> if (addr[i] < 16) take from low
-        //            else take from high
-        // high bit is now based on 4-th bit position
+        __m256i keys = reinterpret_cast<__m256i const &>(offsets);
+        // Step 1: truncate the offsets to the lower 5 bits (index range 0-31).
+        keys = _mm256_and_si256(keys, _mm256_set1_epi8(uint8_t(31)));
+        // Step 2: set the MSB to 1 for keys greater than 15.
+        __m256i high_keys_mask = _mm256_cmpgt_epi8(keys, _mm256_set1_epi8(uint8_t(15)));
+        high_keys_mask = _mm256_and_si256(high_keys_mask, _mm256_set1_epi8(uint8_t(128)));
+        // Step 3: set keys_low to keys where the MSB is set if the corresponding entry is from the high address space.
+        keys_low = _mm256_or_si256(keys, high_keys_mask);
+        // Step 4: set keys_high to keys where the MSB is set if the corresponding entry is from the low address space.
+        keys_high = _mm256_or_si256(keys, _mm256_xor_si256(high_keys_mask,
+                                                           _mm256_set1_epi8(static_cast<uint8_t>(128))));
 
-        // set bit at position 4 to position 7 and mask out all other bits.
-        __m256i tmp = _mm256_and_si256(_mm256_slli_si256(reinterpret_cast<__m256i const &>(offsets), 3),
-                                       reinterpret_cast<__m256i const &>(lane_selector));
-        // add the offset (assume bits not).
-        first_mask = _mm256_or_si256(tmp, reinterpret_cast<__m256i const &>(offsets));
-        second_mask = _mm256_xor_si256(first_mask, reinterpret_cast<__m256i const &>(lane_selector));
-
-        // std::cout << "offsets = "; offsets.print(std::cout) << "\n";
-        // std::cout << "first_mask = "; reinterpret_cast<simd_offset_t const &>(first_mask).print(std::cout) << "\n";
-        // std::cout << "second_mask = "; reinterpret_cast<simd_offset_t const &>(second_mask).print(std::cout) << "\n";
+        // std::cout << "high keys mask = "; reinterpret_cast<simd_offset_t const &>(high_keys_mask).print(std::cout) << "\n";
+        // std::cout << "keys low = "; reinterpret_cast<simd_offset_t const &>(keys_low).print(std::cout) << "\n";
+        // std::cout << "keys high = "; reinterpret_cast<simd_offset_t const &>(keys_high).print(std::cout) << "\n";
     }
 
+    /**
+     * @brief Selects the values from the input array using the offsets from the initialization.
+     *
+     * @tparam value_t The value type for address array.
+     * @param value_array An array containing the values to select from.
+     * @return constexpr __m256i The selected values in a 256-bit register.
+     *
+     * To select the values from the input array the `_mm256_shuffle_epi8` shuffle instruction is used.
+     * However, this instruction can only select values from within a 128-bit lane. Hence, the given values are first
+     * split into two vectors where the first vector stores in both 128-bit lanes the values from the lower 128-bit lane
+     * of the original input values and the second vector stores in both 128-bit lanes the values from the higher
+     * 128-bit lane of the original input values. This is achieved by two calls of `_mm256_permute4x64_epi64`.
+     * Having this, the shuffle instruction can be applied to both vectors separately using the corresponding keys for
+     * the low and high address space.
+     * The result of the two shuffle instructions is then combined using a bitwise or operation.
+     */
     template <typename value_t>
-    constexpr __m256i operator()(address_t<value_t> const & address) const noexcept {
-        // std::cout << "address = "; address[0].print(std::cout) << "\n";
-        __m256i permuted_address = _mm256_permute4x64_epi64(reinterpret_cast<__m256i const &>(address[0]), 0b0100'1110);
-        // std::cout << "permuted address = "; reinterpret_cast<value_t const &>(permuted_address).print(std::cout) << "\n";
+    constexpr __m256i operator()(address_t<value_t> const & value_array) const noexcept {
 
-        // __m256i tmp1 = _mm256_shuffle_epi8(reinterpret_cast<__m256i const &>(address[0]), first_mask);
-        // __m256i tmp2 = _mm256_shuffle_epi8(permuted_address, second_mask);
-        __m256i tmp = _mm256_add_epi8(_mm256_shuffle_epi8(reinterpret_cast<__m256i const &>(address[0]), first_mask),
-                                       _mm256_shuffle_epi8(permuted_address, second_mask));
-        // std::cout << "tmp1 = "; reinterpret_cast<value_t const &>(tmp1).print(std::cout) << "\n";
-        // std::cout << "tmp2 = "; reinterpret_cast<value_t const &>(tmp2).print(std::cout) << "\n";
-        // std::cout << "tmp = "; reinterpret_cast<value_t const &>(tmp).print(std::cout) << "\n";
+        __m256i values = reinterpret_cast<__m256i const &>(value_array[0]);
+        __m256i values_low = _mm256_permute4x64_epi64(values, 0b0100'0100);
+        __m256i values_high = _mm256_permute4x64_epi64(values, 0b1110'1110);
+
+
+        __m256i tmp = _mm256_or_si256(_mm256_shuffle_epi8(values_low, keys_low),
+                                       _mm256_shuffle_epi8(values_high, keys_high));
+        // std::cout << "values_low = "; reinterpret_cast<value_t const &>(values_low).print(std::cout) << "\n";
+        // std::cout << "values_high = "; reinterpret_cast<value_t const &>(values_high).print(std::cout) << "\n";
+        // std::cout << "result = "; reinterpret_cast<value_t const &>(tmp).print(std::cout) << "\n";
         return tmp;
     }
 };
-
-// template <typename simd_offset_t, size_t operand_count>
-//     requires (operand_count > 32 && operand_count <= 64)
-// struct simd_selector<simd_offset_t, selector_tag<operand_count, 8, 256>>
-// {
-//     static constexpr bool in_lane_shuffle = false;
-//     static constexpr size_t max_operand_count = 64;
-
-//     template <typename value_t>
-//     using address_t = std::array<value_t, 2>;
-//     using scalar_offset_t = typename simd_offset_t::value_type;
-
-//     static constexpr simd_offset_t mask1{
-//         0x00,0xF0,0x02,0xF0,0x04,0xF0,0x06,0xF0,0x08,0xF0,0x0A,0xF0,0x0C,0xF0,0x0E,0xF0,
-//         0x10,0xF0,0x12,0xF0,0x14,0xF0,0x16,0xF0,0x18,0xF0,0x1A,0xF0,0x1C,0xF0,0x1E,0xF0,
-//         0x20,0xF0,0x22,0xF0,0x24,0xF0,0x26,0xF0,0x28,0xF0,0x2A,0xF0,0x2C,0xF0,0x2E,0xF0,
-//         0x30,0xF0,0x32,0xF0,0x34,0xF0,0x36,0xF0,0x38,0xF0,0x3A,0xF0,0x3C,0xF0,0x3E,0xF0
-//     };
-
-//     static constexpr simd_offset_t mask2{
-//         0xF0,0x00,0xF0,0x02,0xF0,0x04,0xF0,0x06,0xF0,0x08,0xF0,0x0A,0xF0,0x0C,0xF0,0x0E,
-//         0xF0,0x10,0xF0,0x12,0xF0,0x14,0xF0,0x16,0xF0,0x18,0xF0,0x1A,0xF0,0x1C,0xF0,0x1E,
-//         0xF0,0x20,0xF0,0x22,0xF0,0x24,0xF0,0x26,0xF0,0x28,0xF0,0x2A,0xF0,0x2C,0xF0,0x2E,
-//         0xF0,0x30,0xF0,0x32,0xF0,0x34,0xF0,0x36,0xF0,0x38,0xF0,0x3A,0xF0,0x3C,0xF0,0x3E
-//     };
-
-//     __m512i offsets_even{};
-//     __m512i offsets_uneven{};
-//     __m512i shuffle_mask1{};
-//     __m512i shuffle_mask2{};
-
-//     simd_selector() = delete;
-//     simd_selector(simd_offset_t const & offsets) noexcept
-//     {
-//         offsets_even = _mm512_srli_epi16(reinterpret_cast<__m512i const &>(offsets), 1);
-//         offsets_uneven = _mm512_srli_epi16(reinterpret_cast<__m512i const &>(offsets), 9);
-
-//         shuffle_mask1 = reinterpret_cast<__m512i &&>((offsets & simd_offset_t{static_cast<scalar_offset_t>(1)}) + mask1);
-//         shuffle_mask2 = reinterpret_cast<__m512i &&>((offsets & simd_offset_t{static_cast<scalar_offset_t>(1)}) + mask2);
-//     }
-
-//     template <typename value_t>
-//     constexpr auto operator()(address_t<value_t> const & address) const noexcept
-//     {
-//         // Load the scores in 2x8bit packs from the a and b register.
-//         __m512i tmp_even = _mm512_permutex2var_epi16(reinterpret_cast<__m512i const &>(address[0]),
-//                                                      offsets_even,
-//                                                      reinterpret_cast<__m512i const &>(address[1]));
-
-//         __m512i tmp_uneven = _mm512_permutex2var_epi16(reinterpret_cast<__m512i const &>(address[0]),
-//                                                        offsets_uneven,
-//                                                        reinterpret_cast<__m512i const &>(address[1]));
-
-
-//         // This step is necessary to put the corresponding word
-//         return _mm512_mask_add_epi8(_mm512_shuffle_epi8(tmp_even, shuffle_mask1),
-//                                     0xAAAAAAAAAAAAAAAA,
-//                                     _mm512_shuffle_epi8(tmp_uneven, shuffle_mask2),
-//                                     __m512i{});
-//     }
-// };
 
 // ----------------------------------------------------------------------------
 // epi16
---
+++ test/unit/simd/simd_selector_avx2_test.cpp
@@ -0,0 +1,172 @@
+// -----------------------------------------------------------------------------------------------------
+// Copyright (c) 2006-2021, Knut Reinert & Freie Universität Berlin
+// Copyright (c) 2016-2021, Knut Reinert & MPI für molekulare Genetik
+// This file may be used, modified and/or redistributed under the terms of the 3-clause BSD-License
+// shipped with this file and also available at: https://github.com/rrahn/pairwise_aligner/blob/master/LICENSE.md
+// -----------------------------------------------------------------------------------------------------
+
+#include <gtest/gtest.h>
+
+#include <array>
+#include <algorithm>
+#include <numeric>
+#include <tuple>
+#include <type_traits>
+
+#include <pairwise_aligner/simd/simd_score_type.hpp>
+#include <pairwise_aligner/simd/simd_selector.hpp>
+
+
+namespace pa = seqan::pairwise_aligner;
+
+template <typename test_param_t>
+struct simd_selector_avx2_test : public testing::Test
+{
+    using scalar_value_t = std::tuple_element_t<0, test_param_t>;
+    using scalar_key_t = std::tuple_element_t<1, test_param_t>;
+
+    using simd_value_t = pa::simd_score<scalar_value_t>;
+    using simd_key_t = pa::simd_score<scalar_key_t>;
+
+    template <size_t size_v>
+    using selector_t = pa::simd_selector<simd_value_t, simd_key_t, size_v>;
+
+    void SetUp() override
+    {
+    }
+};
+
+using test_types = ::testing::Types<
+    std::pair<int8_t, uint8_t>
+>;
+
+TYPED_TEST_SUITE(simd_selector_avx2_test, test_types);
+
+
+TYPED_TEST(simd_selector_avx2_test, select_from_9_elements)
+{
+    if constexpr (pa::detail::max_simd_size == 32) {
+        constexpr std::ptrdiff_t element_count_v = 9;
+
+        using selector_t = typename TestFixture::template selector_t<element_count_v>;
+        using simd_value_t = TestFixture::simd_value_t;
+
+        EXPECT_EQ(selector_t::elements_per_select, 16ul);
+
+        using address_t = typename selector_t::address_t;
+
+        // Initialize the container with the data from outside.
+        std::array<typename TestFixture::scalar_value_t, element_count_v> data{1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        address_t converted_data = selector_t::load(data);
+
+        // Select the elements from the following indices.
+        typename TestFixture::simd_key_t select_keys{
+            8, 7, 6, 5, 4, 3, 2, 1,
+            0, 1, 6, 4, 8, 7, 0, 1,
+            1, 1, 0, 7, 8, 8, 5, 2,
+            3, 4, 5, 6, 7, 8, 0, 1
+        };
+
+        auto selector = selector_t::selector_for(select_keys);
+        __m256i actual = selector(converted_data);
+
+        simd_value_t expected_values{
+            9, 8, 7, 6, 5, 4, 3, 2,
+            1, 2, 7, 5, 9, 8, 1, 2,
+            2, 2, 1, 8, 9, 9, 6, 3,
+            4, 5, 6, 7, 8, 9, 1, 2
+        };
+
+        for (size_t i = 0; i < expected_values.size(); ++i) {
+            EXPECT_EQ(static_cast<int>(expected_values[i]),
+                      static_cast<int>(reinterpret_cast<simd_value_t const &>(actual)[i])) << "at index" << i;
+        }
+    } else {
+        GTEST_SKIP() << "Test only available for AVX2.";
+    }
+}
+
+TYPED_TEST(simd_selector_avx2_test, select_from_32_elements_reverse)
+{
+    if constexpr (pa::detail::max_simd_size == 32) {
+        constexpr std::ptrdiff_t element_count_v = 32;
+
+        using selector_t = typename TestFixture::template selector_t<element_count_v>;
+        using simd_value_t = TestFixture::simd_value_t;
+
+        EXPECT_EQ(selector_t::elements_per_select, 32ul);
+
+        using address_t = typename selector_t::address_t;
+
+        // Initialize the container with the data from outside.
+        std::array<typename TestFixture::scalar_value_t, element_count_v> data{
+             1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15, 16,
+            17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32};
+
+        address_t converted_data = selector_t::load(data);
+
+        // Scenario 1: Select the elements in reverse order.
+        typename TestFixture::simd_key_t select_keys{
+            31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16,
+            15, 14, 13, 12, 11, 10,  9,  8,  7,  6,  5,  4,  3,  2,  1,  0
+        };
+
+        auto selector = selector_t::selector_for(select_keys);
+        __m256i actual = selector(converted_data);
+
+        simd_value_t expected_values{
+            32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17,
+            16, 15, 14, 13, 12, 11, 10,  9,  8,  7,  6,  5,  4,  3,  2,  1
+        };
+
+        for (size_t i = 0; i < expected_values.size(); ++i) {
+            EXPECT_EQ(static_cast<int>(expected_values[i]),
+                      static_cast<int>(reinterpret_cast<simd_value_t const &>(actual)[i])) << "at index " << i;
+        }
+
+    } else {
+        GTEST_SKIP() << "Test only available for AVX2.";
+    }
+}
+
+TYPED_TEST(simd_selector_avx2_test, select_from_32_elements_interleaved) {
+        if constexpr (pa::detail::max_simd_size == 32) {
+        constexpr std::ptrdiff_t element_count_v = 32;
+
+        using selector_t = typename TestFixture::template selector_t<element_count_v>;
+        using simd_value_t = TestFixture::simd_value_t;
+
+        EXPECT_EQ(selector_t::elements_per_select, 32ul);
+
+        using address_t = typename selector_t::address_t;
+
+        // Initialize the container with the data from outside.
+        std::array<typename TestFixture::scalar_value_t, element_count_v> data{
+             1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15, 16,
+            17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32};
+
+        address_t converted_data = selector_t::load(data);
+
+        // Scenario 2: Select the elements from interleaved indices.
+        typename TestFixture::simd_key_t select_keys{
+            0, 16, 1, 17,  2, 18,  3, 19,  4, 20,  5, 21,  6, 22,  7, 23,
+            8, 24, 9, 25, 10, 26, 11, 27, 12, 28, 13, 29, 14, 30, 15, 31
+        };
+
+        auto selector = selector_t::selector_for(select_keys);
+        __m256i actual = selector(converted_data);
+
+        simd_value_t expected_values{
+            1, 17,  2, 18,  3, 19,  4, 20,  5, 21,  6, 22,  7, 23,  8, 24,
+            9, 25, 10, 26, 11, 27, 12, 28, 13, 29, 14, 30, 15, 31, 16, 32
+        };
+
+        for (size_t i = 0; i < expected_values.size(); ++i) {
+            EXPECT_EQ(static_cast<int>(expected_values[i]),
+                      static_cast<int>(reinterpret_cast<simd_value_t const &>(actual)[i])) << "at index " << i;
+        }
+    } else {
+        GTEST_SKIP() << "Test only available for AVX2.";
+    }
+}
