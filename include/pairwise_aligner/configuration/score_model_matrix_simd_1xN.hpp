3be11e42da13706ae6cd0f716efa8b9c5f96cacd
rrahn <rene.rahn@fu-berlin.de>
2024-07-10 15:32:16 +0200

    Adapt traversal policy to compute column slice with a profile.
    The row sequence slice for the current column slice of the processed DP matrix tile is converted to a profile score, which is then returned instead of the original sequence slice to the core DP algorithm.
    Ths substitution score then just queries the profile for the given sequence character read from the column sequence.

--- include/pairwise_aligner/configuration/score_model_matrix_simd_1xN.hpp
+++ include/pairwise_aligner/configuration/score_model_matrix_simd_1xN.hpp
@@ -27,7 +27,7 @@
 // #include <pairwise_aligner/matrix/dp_matrix_column_local.hpp>
 #include <pairwise_aligner/matrix/dp_matrix_column.hpp>
 #include <pairwise_aligner/matrix/dp_matrix_lane.hpp>
-// #include <pairwise_aligner/matrix/dp_matrix_lane_profile.hpp>
+#include <pairwise_aligner/matrix/dp_matrix_lane_profile.hpp>
 // #include <pairwise_aligner/matrix/dp_matrix_lane_width.hpp>
 #include <pairwise_aligner/matrix/dp_matrix_local.hpp>
 #include <pairwise_aligner/matrix/dp_matrix.hpp>
@@ -170,7 +170,7 @@ struct traits
         auto make_dp_matrix_policy = [&] () constexpr {
 
             auto default_column = [] () {
-                return dp_matrix::column(dp_matrix::block(dp_matrix::lane));
+                return dp_matrix::column(dp_matrix::block(dp_matrix::lane_profile));
             };
 
             if constexpr (configuration_t::is_local)
--- include/pairwise_aligner/matrix/dp_matrix_lane_profile.hpp
+++ include/pairwise_aligner/matrix/dp_matrix_lane_profile.hpp
@@ -15,6 +15,7 @@
 #include <seqan3/utility/views/slice.hpp>
 
 #include <pairwise_aligner/matrix/dp_matrix_lane.hpp>
+#include <pairwise_aligner/matrix/dp_matrix_lane_width.hpp>
 #include <pairwise_aligner/score_model/strip_width.hpp>
 
 namespace seqan::pairwise_aligner
@@ -23,39 +24,27 @@ inline namespace v1
 {
 namespace dp_matrix {
 
-template <typename dp_block_t, size_t lane_width, bool is_last_lane>
-struct _lane_profile
+namespace _lane_profile {
+
+template <typename last_lane_tag_t, typename ...dp_state_t>
+class _type : public dp_matrix::_lane::_type<last_lane_tag_t, dp_state_t...>
 {
-    class type;
-};
-
-template <typename dp_block_t, size_t lane_width, bool is_last_lane>
-using lane_profile_t = typename _lane_profile<dp_block_t, lane_width, is_last_lane>::type;
-
-template <typename dp_block_t, size_t lane_width, bool is_last_lane>
-class _lane_profile<dp_block_t, lane_width, is_last_lane>::type :
-    public dp_matrix::lane_t<dp_block_t, lane_width, is_last_lane>
-{
-private:
-
-    using base_t = dp_matrix::lane_t<dp_block_t, lane_width, is_last_lane>;
-    using substitution_model_t = typename std::remove_cvref_t<dp_block_t>::substitution_model_type;
-    using profile_t = typename substitution_model_t::template profile_type<lane_width>;
+    using base_t = dp_matrix::_lane::_type<last_lane_tag_t, dp_state_t...>;
+    using substitution_model_t = typename base_t::substitution_model_type;
+    using profile_t = typename substitution_model_t::template profile_type<last_lane_tag_t::width>;
 
     profile_t _profile;
 
 public:
 
-    type() = delete;
-    constexpr explicit type(dp_block_t dp_block, size_t const row_offset) noexcept :
-        base_t{std::forward<dp_block_t>(dp_block), row_offset},
-        _profile{base_t::dp_block().substitution_model().initialise_profile(base_t::row_sequence(),
-                                                                            strip_width<lane_width>)}
-    {
-        // _profile = base_t::dp_block().substitution_model().initialise_profile(base_t::row_sequence(),
-        //                                                                       strip_width<lane_width>);
-    }
-    ~type() = default;
+    _type() = delete;
+    _type(std::ptrdiff_t const offset, dp_state_t ...dp_state) noexcept :
+        base_t{offset, std::forward<dp_state_t>(dp_state)...},
+        _profile{base_t::substitution_model().initialise_profile(base_t::row_sequence(),
+                                                                 strip_width<last_lane_tag_t::width>)}
+    {}
+
+    using base_t::base_t;
 
     constexpr profile_t const & row_sequence() const noexcept
     {
@@ -63,25 +52,23 @@ public:
     }
 };
 
-namespace cpo {
-
-struct _lane_profile_closure
+struct _fn
 {
-    template <typename dp_block_t, size_t lane_width, bool is_last_lane>
-    constexpr auto operator()(dp_block_t && dp_block,
-                              size_t const row_offset,
-                              lane_width_t<lane_width>,
-                              std::bool_constant<is_last_lane>) const noexcept
+    template <typename last_lane_tag_t, typename ...dp_state_t>
+    constexpr auto operator()(last_lane_tag_t const &, std::ptrdiff_t const offset, dp_state_t && ...dp_state)
+        const noexcept
     {
-        using dp_lane_t = dp_matrix::lane_profile_t<dp_block_t, lane_width, is_last_lane>;
-
-        return dp_lane_t{std::forward<dp_block_t>(dp_block), row_offset};
+        using lane_profile_t = _type<last_lane_tag_t, dp_state_t...>;
+        return lane_profile_t{offset, std::forward<decltype(dp_state)>(dp_state)...};
     }
 };
+} // namespace _lane_profile
 
-} // namespace cpo
-} // namespace dp_matrxix
+inline namespace _cpo {
+inline constexpr dp_matrix::_lane_profile::_fn lane_profile{};
 
-inline constexpr dp_matrix::cpo::_lane_profile_closure dp_matrix_lane_profile{};
+} // inline namespace _cpo
+} // namespace dp_matrix
+
 } // inline namespace v1
 }  // namespace seqan::pairwise_aligner
